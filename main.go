32d46040827cecc29a971a38d6d2914d03b00b30
Steve Djumo <steve.djumo@protonmail.com>
2023-12-08 16:20:09 +0100

    Cleaning Dead code

--- main.go
+++ main.go
@@ -14,11 +14,6 @@ import (
 	"gorm.io/gorm"
 )
 
-type inputJson struct {
-	Name string `json:"name"`
-	City string `json:"city"`
-}
-
 type UserCredential struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
@@ -150,26 +145,19 @@ var DB *gorm.DB
 
 func setupRoute(app *fiber.App) {
 	app.Get("/", func(c *fiber.Ctx) error {
-		// text := "Hello World !"
-		input := inputJson{}
-		notOkay := c.BodyParser(&input)
-		if notOkay != nil {
-			return c.Status(fiber.StatusBadRequest).Send([]byte(notOkay.Error()))
-		}
-
-		// return c.Status(fiber.StatusOK).SendString(text)
 		return c.Status(fiber.StatusOK).JSON(fiber.Map{
-			"name": input.Name + " " + input.City,
-			"city": input.City,
+			"hello": "world !",
 		})
 	})
 
+	// ==================================================
+	// ==================================================
+	//                        API
+	// ==================================================
+	// ==================================================
+
 	api := app.Group("/api/v1")
 
-	// Note: JWT Token should only contains data necessary for authorization
-	// on the server. The remaining data can be passed normally
-
-	// TODO: Registration API path
 	api.Post("/registration", func(c *fiber.Ctx) error {
 		user := &UserTable{}
 
@@ -188,15 +176,6 @@ func setupRoute(app *fiber.App) {
 				"message": "User with username '" + user.Username + "' already exists",
 			})
 		}
-
-		// err := func() error {
-		// 	DB.First(existingUser, "username = ?", "grad")
-		// 	return nil
-		// }
-
-		// if err != nil {
-		// 	fmt.Println("DB.Fist() Panic Contained !!!!!!!")
-		// }
 
 		DB.Create(user)
 
@@ -247,64 +226,11 @@ func setupRoute(app *fiber.App) {
 		})
 	})
 
-	api.Post("/test/login", func(c *fiber.Ctx) error {
-		user := UserCredential{}
-
-		if err := c.BodyParser(&user); err != nil {
-			c.Status(fiber.StatusBadGateway).Send([]byte(err.Error()))
-		}
-
-		// Check User in DB
-
-		// If found, send token back to client
-		claims := jwt.MapClaims{
-			"username": user.Username,
-			"password": user.Password,
-			// "exp":      time.Now().Add(time.Minute * 5).Unix(),
-		}
-
-		key := []byte(secret_key)
-		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-		token_string, _ := token.SignedString(key)
-
-		return c.Status(fiber.StatusOK).JSON(fiber.Map{
-			"token": token_string,
-			// "users": users,
-		})
-	})
-
-	api.Get("/test/static_token_extraction", func(c *fiber.Ctx) error {
-		token_string := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDE2MDg2MjQsInBhc3N3b3JkIjoiZWxpb3QiLCJ1c2VybmFtZSI6InN0ZXZlZW5zb24ifQ.mMvXGCSlrhEoidprn8GxW5DpJWWtq_DXD9i4uLPUR4U"
-
-		type Token_Info struct {
-			Username string `json:"username"`
-			Password string `json:"password"`
-			jwt.RegisteredClaims
-		}
-
-		_claims := Token_Info{}
-		token, err := jwt.ParseWithClaims(token_string, &_claims, func(token *jwt.Token) (interface{}, error) {
-			return []byte(secret_key), nil
-		})
-
-		if err != nil || !token.Valid {
-			fmt.Println(err)
-			return c.Status(fiber.StatusBadRequest).JSON(err.Error())
-		}
-
-		return c.Status(fiber.StatusOK).JSON(fiber.Map{
-			"_token": _claims,
-		})
-
-	})
-
-	api.Get("/test/middlewareProtect", jwtMiddlewareProtect, func(c *fiber.Ctx) error {
-		fmt.Print("Inside Middleware Protect TEst")
-
-		return c.Status(fiber.StatusOK).JSON(fiber.Map{
-			"users": c.Locals("users"),
-		})
-	})
+	// ==================================================
+	// ==================================================
+	//                   Middleware
+	// ==================================================
+	// ==================================================
 
 	api.Use(jwtMiddlewareProtect)
 
